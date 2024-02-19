b76cfaa4f8c8296cc4ae5ec401c1cc12cc23a3da
prabhatsingh2003 <ritik2307071@gmail.com>
2024-02-19 11:20:23 +0530

    first commit

---
+++ logic.js
@@ -0,0 +1,22 @@
+const qrinput=document.getElementById('text-input');
+const qrimg=document.getElementById('qr-image');
+const qrbutton=document.getElementById('qr-button');
+
+
+
+console.log(qrinput, qrimg, qrbutton)
+
+qrbutton.addEventListener('click', () => {
+
+    const inputValue = qrinput.value;
+    console.log(inputValue)    
+
+    if( !inputValue ) {
+        alert('Please enter a valid URL');
+        return;
+    }else{
+        qrimg.src= `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`;
+        qrimg.alt = `QR code for ${inputValue}`;
+    }
+
+});
